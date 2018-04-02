d340a72605a8b88ea48330ca760e7df8189b226d
Riccardo Pomato <riccardopomato@hotmail.com>
2018-04-03 00:09:08 +0200

    Add files via upload

--- ConfigureNAT.sh
+++ ConfigureNAT.sh
@@ -7,6 +7,13 @@ sudo -i
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 apt-get -y install iptables-persistent 
+apt-get -y install netcat
+
+echo netcat -l -p 9999 & > /etc/rc.local
+echo exit 0 >> /etc/rc.local
+
+netcat -l -p 9999 &
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 iptables-save > /etc/iptables/rules.v4
@@ -15,9 +22,6 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 200 custom >> /etc/iproute2/rt_tables
 echo auto eth1  >> /etc/network/interfaces.d/50-cloud-init.cfg
 echo iface eth1 inet dhcp  >> /etc/network/interfaces.d/50-cloud-init.cfg
-
-#echo post-up ip rule add from 10.0.2.101 to 168.63.129.16 lookup custom >> /etc/network/interfaces.d/50-cloud-init.cfg
-#echo post-up ip route add default via 10.0.2.1 dev eth1 table custom >> /etc/network/interfaces.d/50-cloud-init.cfg
 
 echo post-up ip rule add from $(getmyip) to 168.63.129.16 lookup custom >> /etc/network/interfaces.d/50-cloud-init.cfg
 echo post-up ip route add default via $1 dev eth1 table custom >> /etc/network/interfaces.d/50-cloud-init.cfg
